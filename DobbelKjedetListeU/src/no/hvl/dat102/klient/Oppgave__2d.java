140b34c9d04e6b9c360fba7824c4a59229acea9f
milky <milky@Milky-s_PC.lan>
2023-02-25 13:59:54 +0000

    Er fortsatt på oppg.2d. Klarer ikke å bruke finn metoden

--- DobbelKjedetListeU/src/no/hvl/dat102/klient/Oppgave__2d.java
+++ DobbelKjedetListeU/src/no/hvl/dat102/klient/Oppgave__2d.java
@@ -1,10 +1,38 @@
 package no.hvl.dat102.klient;
 
+import no.hvl.dat102.kjedet.DobbelKjedetOrdnetListe;
+import no.hvl.dat102.adt.DobbelKjedetOrdnetListeADT;
+import no.hvl.dat102.exceptions.EmptyCollectionException;
+
 public class Oppgave__2d {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String [] navn = new String [5];
+		
+		navn[0] = "Ayla";
+		navn[1] = "Beru";
+		navn[2] = "Igris";
+		navn[3] = "Tank";
+		navn[4] = "Roger";
+		
+		DobbelKjedetOrdnetListe<String> liste = new DobbelKjedetOrdnetListe(new String("AAA"), new String("zzz"));
+		
+		for(int i = 0; i<navn.length; i++) 
+		{
+			liste.leggTil(navn[i] + " ");
+		}
+		
+		System.out.println("Hele listen med alle navnene");
+		System.out.println(liste);
+		
+		
+		
+		System.out.println("\nAlle elementene i listen");	
+		liste.visListe();
+		
+	//	Husk å spørre om hvordan man bruker finn-metoden	
+		// liste.finn("Beru");
+		
 	}
 
 }
